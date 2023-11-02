46482dddb3d948b84a1c022b4e4da40391363304
Daniel Fraga <dan.fraga33@gmail.com>
2023-11-02 16:55:02 +0000

    feat: added neccessary components for task feature

---
+++ src/components/createTaskForm/_taskSelectField.tsx
@@ -0,0 +1,41 @@
+import React, { FC, ReactElement } from 'react';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select, {
+  SelectChangeEvent,
+} from '@mui/material/Select'; //   SelectChangeEvent,
+import { ISelectField } from './interfaces/ISelectField';
+
+export const TaskSelectField: FC<ISelectField> = (
+  props,
+): ReactElement => {
+  const {
+    value = '',
+    label = 'selectBox',
+    name = 'selectBox',
+    items = [{ value: '', label: 'Add Items' }],
+    disabled = false,
+    onChange = (e: SelectChangeEvent) => console.log(e),
+  } = props;
+  return (
+    <FormControl fullWidth size="small">
+      <InputLabel id={`${name}-id`}>{label}</InputLabel>
+      <Select
+        labelId={`${name}-id`}
+        id={`${name}-id-select`}
+        value={value}
+        label={label}
+        name={name}
+        onChange={onChange}
+        disabled={disabled}
+      >
+        {items.map((item, index) => (
+          <MenuItem key={item.value + index} value={10}>
+            {item.label}
+          </MenuItem>
+        ))}
+      </Select>
+    </FormControl>
+  );
+};
